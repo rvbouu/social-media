b54dea05eb9056c81c08e04571f89b001962705e
Vanessa Bou <rvbouu@gmail.com>
2024-05-25 18:39:50 -0500

    debugging GET route for users

--- models/Reaction.js
+++ models/Reaction.js
@@ -19,7 +19,7 @@ const reactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: moment().format('MMMM Do YYYY, h:mm:ss a')
+      get: (timestamp) => moment().format('MMMM Do YYYY, h:mm:ss a')
     }
   },
   {
