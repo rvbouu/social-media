a04fd660efa82f2996801004c57f69d497a050e0
Vanessa Bou <rvbouu@gmail.com>
2024-05-24 22:55:52 -0500

    created Thought.js model

--- models/User.js
+++ models/User.js
@@ -39,6 +39,6 @@ userSchema.virtual('friendCount').get(function () {
   return this.friends.length;
 });
 
-const User = model('User', userSchema);
+const User = model('user', userSchema);
 
 module.exports = User;
