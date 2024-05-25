3e77edf6b38289cc93ed8e93a65b421498a02866
Vanessa Bou <rvbouu@gmail.com>
2024-05-25 16:11:02 -0500

    connected user controller to routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUserById,
+  deleteUserById,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/user-controller');
+
+// /api/users
+// GET and POST route for users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+// GET, PUT, DELETE route for users
+router.route('/:userId').get(getUserById).put(updateUserById).delete(deleteUserById);
+
+// /api/users/:userId/friends/:friendId
+// POST and DELETE route for user's friends list
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
