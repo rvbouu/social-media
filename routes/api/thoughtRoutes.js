a7af4d61f90c9634c4193280eac55625837954a0
Vanessa Bou <rvbouu@gmail.com>
2024-05-25 16:19:40 -0500

    connected thought controller to routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+  getThoughts,
+  getThoughtById,
+  createThought,
+  updateThoughtById,
+  deleteThoughtById,
+  addReaction,
+  deleteReaction
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+// GET and POST route for thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+// GET, PUT, DELETE route for thoughts
+router.route('/:thoughtId').get(getThoughtById).put(updateThoughtById).delete(deleteThoughtById);
+
+// /api/thoughts/:thoughtId/reactions
+// POST route for thought reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+// DELETE route for thought reactions
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
